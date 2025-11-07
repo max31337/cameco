747e92c021ccf0eb0a891b06a480970fe105e9f1
max31337 <navarro.markanthony.tud@gmail.com>
2025-11-08 00:07:10 +0800

    theme toggle on top right corner

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -1,6 +1,7 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
+import AppearanceToggleDropdown from '@/components/appearance-dropdown';
 
 export function AppSidebarHeader({
     breadcrumbs = [],
@@ -9,9 +10,13 @@ export function AppSidebarHeader({
 }) {
     return (
         <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/50 bg-background px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
-            <div className="flex items-center gap-2">
-                <SidebarTrigger className="-ml-1" />
-                <Breadcrumbs breadcrumbs={breadcrumbs} />
+            <div className="flex w-full items-center justify-between gap-2">
+                <div className="flex items-center gap-2">
+                    <SidebarTrigger className="-ml-1" />
+                    <Breadcrumbs breadcrumbs={breadcrumbs} />
+                </div>
+                {/* Theme Toggle in top right corner */}
+                <AppearanceToggleDropdown />
             </div>
         </header>
     );
